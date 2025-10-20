3a44b13e109f2fa5adac72a1e52e33163b2d1f45
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-10-20 15:10:15 +0100

    feat: link web app manifest in _document.tsx

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link to the Web App Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        
+        {/* Set the theme color for the browser UI */}
+        <meta name="theme-color" content="#FFFFFF" /> 
+        
+        {/* Apple Touch Icon for iOS devices */}
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png"></link>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
